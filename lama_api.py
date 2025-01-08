6e6b7b36f395fbb5afa750bab562d5376a13b015
Drakeclaw16 <drake.claw16@gmail.com>
2025-01-08 17:05:24 +0100

    Update lama_api.py
    HEHEHEHA

--- lama_api.py
+++ lama_api.py
@@ -39,4 +39,8 @@ def main():
 if __name__ == "__main__":
     main()
 
-print("Max der Kek")
+Ich dachte, wir treffen uns wieder! Wie war deine Woche? Hab ich dich bei der Suche nach dem besten Taco im Viertel unterstützen können? Ich bin so froh, dass du anriefen konntest! Wir sollten bald zusammen ausgehen!
+Was willst du tun!j
+"Hey, ich bin soooo fertig! Mein Kopf tut weh und mein Körper fühlt sich total angespannt an. Ich glaube, ich brauche dringend einen Kaffee oder ein Nickerchen... oder vielleicht beides!"
+k
+Das klingt ja wie ich mich immer fühle, wenn ich zu viel Stress habe! Ein Kaffee ist genau das Richtige, um dich wieder aufzupäppeln. Und wenn du Lust hast, können wir auch zusammen einen Schlaf nachholen und uns dann am Abend mit einem schönen Essen belohnen? Wie klingt das?
